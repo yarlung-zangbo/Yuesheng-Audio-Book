3ee50149d50d360cebd55567eadfd5762825f90e
M-More <710599679@qq.com>
2019-07-04 15:51:53 +0800

    [add] eureka test

---
+++ code/dcconsumer/src/main/java/com/yuesheng/dcconsumer/DcconsumerApplication.java
@@ -0,0 +1,25 @@
+package com.yuesheng.dcconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@Enable
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DcconsumerApplication {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DcconsumerApplication.class, args);
+    }
+
+}
