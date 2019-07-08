90087d332cbfb6823c2124138f2fd9187804cb75
M-More <710599679@qq.com>
2019-07-08 13:57:19 +0800

    [add] 7.5 daily conclusion

--- code/ribbonweb/src/main/java/com/yuesheng/ribbonweb/IMyFeignClient.java
+++ code/ribbonweb/src/main/java/com/yuesheng/ribbonweb/IMyFeignClient.java
@@ -6,13 +6,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value="ribbon-client")
+@FeignClient(value="client1")
 public interface IMyFeignClient {
 
     @RequestMapping(value = "/{id}",method = RequestMethod.GET)
     public String test(@PathVariable("id") Long id);
-
-    @RequestMapping(value = "/post",method = RequestMethod.POST)
-    public String post(@RequestBody TestParam testParam);
-
 }
